096e8d8747be22dd7709a44642a89a84bea1301b
MatisDembele <109804180+MatisDembele@users.noreply.github.com>
2023-01-31 19:29:16 +0100

    1ère version

---
+++ PileObjet/Pile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MesOutils
+{
+    /// <summary>
+    /// Pile contenant pouvant contenir des entiers
+    /// On ajoute après le dernier élément ajouté
+    /// On retire toujours le dernier élément ajouté
+    /// </summary>
+    class Pile
+    {
+        /// <summary>
+        /// Nombre maximum d'élements de la Pile
+        /// </summary>
+        private int nbMaxElt;
+
+        /// <summary>
+        /// Liste contenant les élements de la Pile
+        /// </summary>
+        private List<int> elements;
+
+        public Pile(int nbMaxElt)
+        {
+            this.nbMaxElt = nbMaxElt;
+            this.elements = new List<int>();
+        }
+
+        public bool PileVide()
+        {
+            return this.elements.Count == 0;
+        }
+
+        public bool PilePleine()
+        {
+            return this.elements.Count-1 == this.nbMaxElt;
+        }
+
+        public void Empiler(int nb)
+        {
+            if (!(this.PilePleine()))
+            {
+                this.elements.Add(nb);
+            }
+            else
+            {
+                throw new Exception("Impossible d'empiler,pile pleine");
+            }
+        }
+
+        public int Depiler()
+        {
+            int res = (int)this.elements[elements.Count - 1];
+            this.elements.RemoveAt(this.elements.Count - 1);
+            return res;
+        }
+            
+    }
+}
--- PileObjet/Program.cs
+++ PileObjet/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 
-namespace PileObjet
+namespace MesOutils
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestePileVidePleine(5);
+            TestePileVidePleine(0);
+            TesteEmpiler(5);
+            //TesteEmpiler(2);
+            TesteEmpilerDepiler(5);
+            Console.WriteLine("Fin du progamme");
+            Console.ReadKey();
+        }
+
+        static void TestePileVidePleine(int nbElements)
+        {
+            Pile unePile = new Pile(10);
+            if (unePile.PileVide())
+            {
+                Console.WriteLine("la pile est vide");
+            }
+            else
+            {
+                Console.WriteLine("la pile n'est pas vide");
+            }
+            if (unePile.PilePleine())
+            {
+                Console.WriteLine("la pile est pleine");
+            }
+            else
+            {
+                Console.WriteLine("la pile n'est pas pleine");
+            }
+        }
+
+        static void TesteEmpiler(int nbElements)
+        {
+            //try
+            //{
+            Pile unePile = new Pile(nbElements);
+            unePile.Empiler(2);
+            unePile.Empiler(14);
+            unePile.Empiler(6);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+        }
+        static void TesteEmpilerDepiler(int nbElements)
+        {
+            try
+            {
+                Pile unePile = new Pile(nbElements);
+                unePile.Empiler(2);
+                unePile.Empiler(22);
+                int valeurDepilee = (int)unePile.Depiler();
+                Console.WriteLine("valeur dépilée : " + valeurDepilee);
+                unePile.Empiler(17);
+                while (!unePile.PileVide())
+                {
+                    valeurDepilee = (int)unePile.Depiler();
+                }
+                unePile.Depiler();
+                Console.WriteLine(valeurDepilee);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
