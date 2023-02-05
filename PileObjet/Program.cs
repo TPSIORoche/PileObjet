ac38344f886c1d74d521662281664aac83c1ac28
MatisDembele <109804180+MatisDembele@users.noreply.github.com>
2023-02-05 19:13:14 +0100

    Classe Pile modifié, Classe UtilitaireConsole modifiée et classe TetsePile modifiée

--- PileObjet/Program.cs
+++ PileObjet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Utilitaires;
+using PileObjet.Tests;
 
 namespace MesOutils
 {
@@ -11,111 +12,19 @@ namespace MesOutils
             //TestePileVidePleine(0);
             //TesteEmpiler(5);
             //TesteEmpiler(2);
-            TesteEmpilerDepiler();
+            //TestPile.TesteEmpilerDepiler();
             //TestNbSaisi();
             //Console.WriteLine(Convertir(10,16));
+            TestPile.TestePileVide();
+            TestPile.TesteEmpiler();
+            TestPile.TesteEmpilerDepiler();
+            TestPile.TesteConversion();
+            TestPile.TesteConversion(154, 2);
+            TestPile.TesteConversion(11, 16);
+            TestPile.TesteConversion(2986, 16);
+            TestPile.TesteConversion(9999, 16);
             Console.WriteLine("Fin du progamme");
             Console.ReadKey();
         }
-
-        //static void TestePileVidePleine(int nbElements)
-        //{
-        //    Pile<int> unePile = new Pile<int>(10);
-        //    if (unePile.PileVide())
-        //    {
-        //        Console.WriteLine("la pile est vide");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("la pile n'est pas vide");
-        //    }
-        //    if (unePile.PilePleine())
-        //    {
-        //        Console.WriteLine("la pile est pleine");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("la pile n'est pas pleine");
-        //    }
-        //}
-
-        //static void TesteEmpiler(int nbElements)
-        //{
-        //    //try
-        //    //{
-        //    Pile unePile = new Pile(nbElements);
-        //    unePile.Empiler(2);
-        //    unePile.Empiler(14);
-        //    unePile.Empiler(6);
-        //    //}
-        //    //catch(Exception ex)
-        //    //{
-        //    //    Console.WriteLine(ex.Message);
-        //    //}
-        //}
-        static void TesteEmpilerDepiler()
-        {
-            try
-            {
-                Pile <string>unePile = new Pile<string>();
-                unePile.Empiler("A");
-                unePile.Empiler("BB");
-                string valeurDepilee = unePile.Depiler();
-                Console.WriteLine("valeur dépilée : " + valeurDepilee);
-                unePile.Empiler("AH");
-                Console.WriteLine("valeur dépilée : " + valeurDepilee);
-
-                while (!unePile.PileVide())
-                {
-                    valeurDepilee = unePile.Depiler();
-                }
-                unePile.Depiler();
-                Console.WriteLine(valeurDepilee);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        static void TestNbSaisi()
-        {
-            int nbSaisi = Utilitaire.SaisirNb();
-            Console.WriteLine($"Nombre saisi : {nbSaisi}");
-            nbSaisi = Utilitaire.SaisirNb(10);
-            Console.WriteLine($"Nombre saisi : {nbSaisi}");
-            nbSaisi = Utilitaire.SaisirNb(10,30);
-            Console.WriteLine($"Nombre saisi : {nbSaisi}");
-        }
-        //static string Convertir(int pNbAConvertir, int pNewbase)
-        //{
-        //    Pile<int> unePile = new Pile<int>();
-        //    int a = pNbAConvertir;
-        //    int r;
-        //    string resultat = "";
-
-        //    while (a > 0)
-        //    {
-        //        r = a % pNewbase;
-        //        unePile.Empiler(r);
-        //        a /= pNewbase;
-        //    }
-        //    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        //    while (!unePile.PileVide())
-        //    {
-        //        //if ((int)unePile.tabEle[unePile.tabElem.Count - 1] > 9)
-        //        if (unePile.()>9)
-        //        {
-        //            int alte = ((int)unePile.Depiler() % pNewbase) - 10;
-        //            resultat += alpha[alte];
-        //        }
-        //        else
-        //        {
-        //            resultat += unePile.Depiler().ToString();
-        //        }
-        //    }
-        //    return resultat;
-        //}
-
     }
 }
---
+++ PileObjet/Tests/Test.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MesOutils;
+using Utilitaires;
+
+namespace PileObjet.Tests
+{
+    internal abstract class TestPile
+    {
+        public static void TestePileVide()
+        {
+            Pile<int> unePile = new Pile<int>();
+            if (unePile.PileVide())
+            {
+                Console.WriteLine("la pile est vide");
+            }
+            else
+            {
+                Console.WriteLine("la pile n'est pas vide");
+            }
+        }
+
+        public static void TesteEmpiler()
+        {
+            try
+            {
+                Pile<int> unePile = new Pile<int>();
+                unePile.Empiler(2);
+                unePile.Empiler(14);
+                unePile.Empiler(6);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void TesteEmpilerDepiler()
+        {
+            try
+            {
+                Pile<string> unePile = new Pile<string>();
+                unePile.Empiler("A");
+                unePile.Empiler("BB");
+                string valeurDepilee = unePile.Depiler();
+                Console.WriteLine("valeur dépilée : " + valeurDepilee);
+                unePile.Empiler("AH");
+                Console.WriteLine("valeur dépilée : " + valeurDepilee);
+
+                while (!unePile.PileVide())
+                {
+                    valeurDepilee = unePile.Depiler();
+                }
+                unePile.Depiler();
+                Console.WriteLine(valeurDepilee);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void TestNbSaisi()
+        {
+            int nbSaisi = Utilitaire.SaisirNb();
+            Console.WriteLine($"Nombre saisi : {nbSaisi}");
+            nbSaisi = Utilitaire.SaisirNb(10);
+            Console.WriteLine($"Nombre saisi : {nbSaisi}");
+            nbSaisi = Utilitaire.SaisirNb(10, 30);
+            Console.WriteLine($"Nombre saisi : {nbSaisi}");
+        }
+
+        //public static string TesteConversion()
+        //{
+        //    int a = Utilitaire.SaisirNb(0);
+        //    int b = Utilitaire.SaisirNb(1,17);
+        //    return Utilitaire.Convertir(a, b);
+        //}
+
+        //public static string TesteConversion(int pNbAConvertir, int pNewbase)
+        //{
+        //    return Utilitaire.Convertir(pNbAConvertir,pNewbase);
+        //}
+
+        public static void TesteConversion()
+        {
+            Console.WriteLine("Saisie du nombre à convertir. ");
+            int nbAConvertir = Utilitaire.SaisirNb(0);
+            Console.WriteLine("Saisie de la nouvelle base. ");
+            int newBase = Utilitaire.SaisirNb(2, 16);
+            TesteConversion(nbAConvertir, newBase);
+        }
+        public static void TesteConversion(int nbAConvertir,int newBase)
+        {
+            try
+            {
+                string valeurConvertie = Utilitaire.Convertir(nbAConvertir, newBase);
+                Console.WriteLine("ValeurConvertie convertie : " + valeurConvertie);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
